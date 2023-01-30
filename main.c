8e767c52af3e5d4c62b04d2fc88a339dde826aca
Nabile Sall <idrissa.sall@etu.univ-lyon1.fr>
2023-01-30 23:13:13 +0100

    Ajout des fichiers c et le fichier csv de test

---
+++ main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "fonction.h"
+#define TAILLE_MAX 256
+
+int main(void){
+    remplir(0,0,tableau);/*on remplit le tableau dès le lancement du programme*/
+    menu();
+    return 0;
+}
+
+/*
+test
+normes: c90 et -wall pour les warnings
+*/
+
+
+
+
+
+
+
